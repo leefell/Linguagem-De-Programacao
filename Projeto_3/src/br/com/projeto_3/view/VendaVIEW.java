1c55e077939993d9bbc06a7542e92940215c558a
Alexandre <129619218+leefell@users.noreply.github.com>
2024-06-19 09:04:39 -0300

    Update VendaVIEW.java
    fix

--- Projeto_3/src/br/com/projeto_3/view/VendaVIEW.java
+++ Projeto_3/src/br/com/projeto_3/view/VendaVIEW.java
@@ -96,7 +96,7 @@ public class VendaVIEW extends javax.swing.JInternalFrame {
                         Integer.parseInt(String.valueOf(
                         jtl_consultar_pro_selecionado.getValueAt(i, 3)));
             }
-            TotalVenda.setText(String.format("%.2f", total));
+            TotalVenda.setText(String.valueOf(total));
         }catch(Exception erTab){
             System.out.println("Erro SQL: " + erTab);
         }
